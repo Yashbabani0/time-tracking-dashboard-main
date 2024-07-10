7b345943d90841e50d78a68f5486a4b9920d26c1
Yash Babani <yashbabani09@gmail.com>
2024-07-11 02:38:26 +0530

    Tailwind config colors and fonts add

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -4,7 +4,7 @@ export default function Profile() {
   return <div className="rounded-xl bg-[] w-[4em] h-[4em]">
     <div className="part1">
       <div className="profileImg">
-test
+        
       </div>
       <div className="profileData">
 
