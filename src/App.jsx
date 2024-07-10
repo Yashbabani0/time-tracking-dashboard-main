695d9cfcc97172e2916a8738a1829cddd91bb303
Yash Babani <yashbabani09@gmail.com>
2024-07-10 23:20:23 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Profile from './Components/Profile'
+import Databox from './Components/Databox'
 
 export default function App() {
   return (
     <div>
-      
+      <Profile />
+      <Databox />
     </div>
   )
 }
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Profile() {
+  return <div className="rounded-xl bg-[] w-[4em] h-[4em]">
+    <div className="part1">
+      <div className="profileImg">
+test
+      </div>
+      <div className="profileData">
+
+      </div>
+    </div>
+    <div className="part2">
+
+    </div>
+  </div>;
+}
